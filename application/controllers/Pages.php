6cc48d2f21914eb0715ef7bccae6bd716c94add4
Asri Comel <asrihafiz00@gmail.com>
2023-01-01 22:28:36 +0800

    update pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -12,7 +12,7 @@
             $data['title'] = ucfirst($page);
 
             $this->load->view('templates/header');
-            $this->load->view('pages'.$page, $data);
-            $this->load->view('templates/header');
+            $this->load->view('pages/'.$page, $data);
+            $this->load->view('templates/footer');
 	    }
-}
+    }
