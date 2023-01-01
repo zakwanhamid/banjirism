95c93630643109af3c6003bb75b442f9a21051a0
zakwanhamid <zakwanhamid@gmail.com>
2023-01-01 22:05:41 +0800

    remove navbar

--- application/views/register.php
+++ application/views/register.php
@@ -43,40 +43,6 @@
 
 </head>
 <body>
-  <nav class="navbar navbar-expand-md navbar-light " style="background:#5690e9">
-      <div class="container-xxl">
-        <a href="index.html#intro" class="navbar-brand">
-          <img src="/assets/logoz.png"  height="50" alt="logo">
-        </a>
-        <!-- toggle buttom for mobile nav -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-        data-bs-target="#main-nav" aria controls="main-nav" aria-expanded="false"
-        aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <!-- navbar links -->
-        <div class="collapse navbar-collapse justify-content-end align-center fw-bold" id="main-nav">
-          <ul class="navbar-nav">
-            <li class="nav-item ">
-              <a class="nav-link" href="index.php#intro">Home</a>
-            </li>
-            <li class="nav-item ms-lg-4">
-              <a class="nav-link" href="index.php#aboutUs">About Us</a>
-            </li>
-            <li class="nav-item ms-lg-4">
-              <a class="nav-link" href="index.php#eBantuan">e-Bantuan</a>
-            </li>
-            <li class="nav-item ms-lg-4">
-              <a class="nav-link" href="login.php">Log In</a>
-            </li>
-            <li class="nav-item ms-lg-4">
-              <a class="nav-link" href="register.php">Register</a>   
-            </li>
-          </ul>
-        </div>
-      </div> 
-    </nav>
 
     <section id="register">
       <div class="container form my-4 mx-5">
