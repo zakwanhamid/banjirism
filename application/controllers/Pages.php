2e0ed7ea11800091f970dd6b93e466e61a3e3c4f
Asri Comel <asrihafiz00@gmail.com>
2023-01-01 23:10:51 +0800

    try

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,7 +4,7 @@
 
     	public function view($page = 'home')
 	    {
-            if(!file_exists(APPPATH.'views/pages/'.$page.'.php'))
+            if(!file_exists( APPPATH. 'views/pages/'.$page.'.php'))
             {
                 show_404();
             }
