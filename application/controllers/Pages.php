63d94abb4048dfe14891cfc7882c5e566d10dd8e
Asri Comel <asrihafiz00@gmail.com>
2023-01-01 22:23:18 +0800

    update pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,18 +1,18 @@
 <?php
 
-class Pages extends CI_Controller {
+    class Pages extends CI_Controller {
 
-	public function view($page = 'home')
-	{
-        if(!file_exists(APPPATH.'views/pages/'.$page.'.php'))
-        {
-            show_404();
-        }
+    	public function view($page = 'home')
+	    {
+            if(!file_exists(APPPATH.'views/pages/'.$page.'.php'))
+            {
+                show_404();
+            }
 
-        $data['title'] = ucfirst($page);
+            $data['title'] = ucfirst($page);
 
-        $this->load->view('template/header');
-        $this->load->view('pages'.$page, $data);
-        $this->load->view('template/header');
-	}
+            $this->load->view('templates/header');
+            $this->load->view('pages'.$page, $data);
+            $this->load->view('templates/header');
+	    }
 }
