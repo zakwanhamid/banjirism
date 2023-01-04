71766e2be0e9fd58598fc50be305459929c0b3c3
zakwanhamid <zakwanhamid@gmail.com>
2023-01-04 19:55:46 +0800

    add user controller

---
+++ application/controllers/Users.php
@@ -0,0 +1,22 @@
+<?php
+  class Users extends CI_Controller{
+    public function register(){
+      $data['title'] = 'Sign Up';
+
+      $this form_validation->set_rules('name_ngo_name', 'Name', 'required');
+      $this form_validation->set_rules('user_name', 'Username', 'required');
+      $this form_validation->set_rules('user_password', 'Password', 'required');
+      $this form_validation->set_rules('user_password', 'Confirm Password', 'required');
+      $this form_validation->set_rules('name', 'Name', 'matches[password]');
+
+      if($this->form_validation->run() === FALSE ){
+        $this->load->view('templates/header');
+        $this->load->view('users/register', $data);
+        $this->load->view('templates/footer');
+
+      }else{
+        die('Continue');
+      }
+
+    }
+  }
