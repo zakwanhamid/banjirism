3301ac8c9fadb7268b2d4b2443352dfbe5f6ce94
Asri Comel <asrihafiz00@gmail.com>
2023-01-27 15:04:08 +0800

    update database MVC

---
+++ application/views/management/create_stock_item_comm.php
@@ -0,0 +1,22 @@
+<h2><?= $title;?> </h2>
+<?php echo validation_errors(); ?>
+<?php echo form_open("Energies/create"); ?>
+<form>
+  <div class="mb-3">
+    <label>Energies ID</label>
+    <input type="text" class="form-control" name="energies_id" placeholder="Add Energies ID">
+  </div>
+  <div class="mb-3">
+    <label>Location</label>
+    <input type="text" class="form-control" name="location_energies" placeholder="Add Location">
+  </div>
+  <div class="mb-3">
+    <label>Number Member</label>
+    <input type="text" class="form-control" name="num_member_energies" placeholder="Add Number Member">
+  </div>
+  <div class="mb-3">
+    <label>Phone Number</label>
+    <input type="text" class="form-control" name="phone_number_energies" placeholder="Add Phone Number">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
