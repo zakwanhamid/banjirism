b1e8a89b95f5e6507b04988d988b3a8c08da9675
Asri Comel <asrihafiz00@gmail.com>
2023-01-28 19:05:50 +0800

    update status ngo

--- application/controllers/Status_ngo.php
+++ application/controllers/Status_ngo.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Status_comm extends CI_Controller {
+class Status_ngo extends CI_Controller {
 
 	public function index()
 	{
-        $data['title'] = "Status comm";
+        $data['title'] = "Status NGO";
 
         
 		$this->load->model('StatusCommModel');
-        $data['status_comm'] = $this->StatusCommModel->get_StatusComm();
+        $data['status_ngo'] = $this->StatusCommModel->get_StatusComm();
 
 		$this->load->view('templates/header',$data);
-		$this->load->view('management/status_comm');
+		$this->load->view('management/status_ngo');
 		$this->load->view('templates/footer');
 	}
 
