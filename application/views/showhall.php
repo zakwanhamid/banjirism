0d3f06b8728d96738574e32338d5c5b8c36700dc
Asri Comel <asrihafiz00@gmail.com>
2023-01-28 23:15:24 +0800

    update showhall

--- application/views/showhall.php
+++ application/views/showhall.php
@@ -1,47 +1,3 @@
-<!-- <style>
-
-</style>
-<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-<script src="http://maps.google.com/maps/api/js?key=AIzaSyAtPAcmfk6Vbg4vQSO2OG9Hbh1KFhzyTPc"
-  type="text/javascript"></script>
-<div id="map">
-
-  <script type="text/javascript">
-    var locations = [
-      ['Taman Pekaka Community Hall', 5.350564426916976, 100.29450070734104, 4],
-      ['Taman Tiram Community Hall', 5.306729579139892, 100.27385227369517, 5],
-      ['Jalan Tokong Batu Community Hall', 5.395213254399922, 100.3184165057981, 3],
-      ['Tanjung Bungah Community Hall', 5.460783565485189, 100.28121607993431, 2],
-      ['Lebuh Macallum Community Hall', 5.407100095633074, 100.33402562128721, 1]
-    ];
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: new google.maps.LatLng(5.40, 100.335),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function (marker, i) {
-        return function () {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
-  </script>
-
-</div> -->
-
 <style>
   #map {
     width: 100%;
@@ -79,7 +35,7 @@
   var infoWindowContent = [
         <?php foreach ($data as $locId) { ?>
       ['<div class="info_content">'
-        + '<h3><?php echo $locId->hallName; ?></h3>' + '<p><?php echo $locId->hallID; ?></p>' + '<div>'],
+        + '<h3><?php echo $locId->hallName; ?></h3>' + '<p><?php echo $locId->hallID; ?></p>'+ '<a class="btn btn-primary" href="<?php echo base_url('energies/index'); ?>" role="button">Link</a>' +'<div>'],
         <?php } ?>
       ];
 
