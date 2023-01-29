6e6d66d053a95cb724ef1038f998a715e6c27d5f
Ezzaty Nordin <nurezzatynordin@gmail.com>
2023-01-29 18:42:48 +0800

    Update Showhall Title

--- application/controllers/Showhall.php
+++ application/controllers/Showhall.php
@@ -15,9 +15,9 @@ class Showhall extends CI_Controller
   public function index()
   {
     $data = $this->db->select('*')->from('communityhall')->get()->result();
-    //$data['title'] = "Community Hall";
+    $data['title'] = "Community Hall";
     $title = "Community Hall";
-    $this->load->view('templates/header', $title);
+    $this->load->view('templates/header', $data);
     $this->load->view('showhall', ['data' => $data]);
     $this->load->view('templates/footer');
   }
