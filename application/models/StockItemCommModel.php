bde3d85735c7b1d76424cbcd9f8303ea44b584de
aimanrozali <aimanrozali@student.usm.my>
2023-01-31 03:37:35 +0800

    update commhall view

--- application/models/StockItemCommModel.php
+++ application/models/StockItemCommModel.php
@@ -1,43 +1,54 @@
 <?php
-    class StockItemCommModel extends CI_Model
+class StockItemCommModel extends CI_Model
+{
+    public function __construct()
     {
-        public function __construct()
-        {
-            $this->load->database();
-            parent::__construct();
-        }
-
-        public function get_StockItemComm()
-        {
-            $this->db->select('*');
-            $this->db->from('item');
-            $this->db->join('item_category', 'item.item_CAT_id = item_category.item_CAT_id');
-            $query = $this->db->get();
-            return $query->result_array();
-
-        }
-
-        public function get_item_requests($item_id) {
-            $this->db->select('*');
-            $this->db->from('item_request');
-            $this->db->where('item_id', $item_id);
-            $query = $this->db->get();
-            return $query->result();
-          }
-
-        public function getItem()
-        {
-            return $this->db->get('item_category')->result();
-        }
-
-        public function insertItem1($query)
-        {
-            $this->db->insert('item',$query);
-        }
-
-        public function insertItem2($query)
-        {
-            $this->db->insert('item_request',$query);
-        }
+        $this->load->database();
+        parent::__construct();
     }
-    ?>
+
+    public function get_StockItemComm()
+    {
+        $this->db->select('*');
+        $this->db->from('item');
+        $this->db->join('item_category', 'item.item_CAT_id = item_category.item_CAT_id');
+        $query = $this->db->get();
+        return $query->result_array();
+
+    }
+
+    public function get_item_requests($item_id)
+    {
+        $this->db->select('*');
+        $this->db->from('item_request');
+        $this->db->where('item_id', $item_id);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getItem()
+    {
+        return $this->db->get('item_category')->result();
+    }
+
+    public function insertItem1($query)
+    {
+        $this->db->insert('item', $query);
+    }
+
+    public function insertItem2($query)
+    {
+        $this->db->insert('item_request', $query);
+    }
+
+    public function getHall($id)
+    {
+        $this->db->select('*');
+        $this->db->from('communityhall');
+        $this->db->where('comm_id', $id);
+        $qr = $this->db->get();
+
+        return $qr->result();
+    }
+}
+?>
