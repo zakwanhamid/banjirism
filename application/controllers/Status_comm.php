440508a0dc94ac214de5ae77aa8c6419001b0932
Alif <alifhilman3@gmail.com>
2023-01-30 23:09:10 +0800

    update name of status comm

--- application/controllers/Status_comm.php
+++ application/controllers/Status_comm.php
@@ -8,8 +8,8 @@ class Status_comm extends CI_Controller {
         $data['title'] = "Status comm";
 
         
-		$this->load->model('StatusCommModel');
-        $data['status_comm'] = $this->StatusCommModel->get_StatusComm();
+		$this->load->model('ItemRequestModel');
+        $data['status_comm'] = $this->ItemRequestModel->get_StatusComm();
 
 		$this->load->view('templates/header',$data);
 		$this->load->view('management/status_comm');
@@ -21,12 +21,12 @@ class Status_comm extends CI_Controller {
 	    $data['title'] = "create request item";
 
 		$this->load->database();
-		$this->load->model('StatusCommModel');
+		$this->load->model('ItemRequestModel');
 	    $this->load->helper(array('form', 'url'));
 
 	    $this->load->library('form_validation');
 
-		$data['item_name'] = $this->StatusCommModel->getItemName();
+		$data['item_name'] = $this->ItemRequestModel->getItemName();
 
 		$this->load->view('templates/header',$data);
 		$this->load->view('management/create_request', $data);
@@ -36,13 +36,13 @@ class Status_comm extends CI_Controller {
 
 	public function createItem() {
 
-		$this->load->model('StatusCommModel');
+		$this->load->model('ItemRequestModel');
 
 		$data1 = array( 
 			'quantity_request' => $this->input->post('quantity_request'), 'item_name' => $this->input->post('item_name'), 'item_desc' => $this->input->post('item_desc')
 		);
 
-		$this->StatusCommModel->insertItem($data1);
+		$this->ItemRequestModel->insertItem($data1);
 
 	}
 
